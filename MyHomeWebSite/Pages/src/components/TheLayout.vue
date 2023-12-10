4db1bd433eec43dfece20d83b188e48a5da47fc5
ken <cd.liu0607@gmail.com>
2023-12-10 21:41:00 +0800

    build

---
+++ MyHomeWebSite/Pages/src/components/TheLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+	message: String,
+	count: Number,
+})
+</script>
+
+<template>
+	<div class="son">
+		<h3>子組件Son</h3>
+		<h1>{{ message }} - {{ count }}</h1>
+	</div>
+</template>
+
+<style scoped>
+.son {
+	background-color: aqua;
+}
+</style>
