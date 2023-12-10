c121c6f3a730e98d840f682530cb459b6f7fbb42
ken <cd.liu0607@gmail.com>
2023-12-10 21:41:00 +0800

    testbuild

---
+++ MyHomeWebSite/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using MyHomeWebSite.Model;
+using System.Collections.Generic;
+
+namespace MyHomeWebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Index()
+        {
+            IModel<IndexModel> model = new IndexModel();
+
+            TestModel<IndexModel> test = new TestModel<IndexModel>();
+
+            List<int> ss = new List<int>();
+            ss.Where(x => x > 0).ToList();  
+
+            test.Success(m => m.Tax);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MyHomeWebSite/Pages/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheLayout from './components/TheLayout.vue'
+import { ref } from 'vue'
+const count = ref(100)
+</script>
+
+<template>
+	<div class="father">
+		<h2>父組件App</h2>
+		<TheLayout
+			:count="count"
+			message="father message" />
+	</div>
+</template>
+
+<style scoped></style>
